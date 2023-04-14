1adfaa80b5e7ec5c97df467d3a112c4ea6017322
VidwaDeSeram <vidwadeseram2002@gmail.com>
2023-04-15 02:23:28 +0530

    room management API connected

--- src/components/UserSearch.js
+++ src/components/UserSearch.js
@@ -1,28 +1,65 @@
-import React, { useState } from 'react';
-import './UserSearch.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./UserSearch.css";
 
 const UserSearch = () => {
-    const [searchTerm, setSearchTerm] = useState('');
+  const [studentId, setstudentId] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
-    const handleInputChange = (event) => {
-        setSearchTerm(event.target.value);
-    };
+  const handleInputChange = (event) => {
+    setstudentId(event.target.value);
+  };
+  const handleLogin = async () => {
+    // Your login or sign-up logic here
+    // After a successful login or sign-up, save the token to the localStorage
+    const token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDM1MDJjMzY5MjI4MTk0MDE2NjY2MmQiLCJpYXQiOjE2ODE0OTk2NDAsImV4cCI6MTY4MTUwMzI0MH0.C19WWZjfugRjuPeyPpuGYoWJDak2OunnFzcIuKW5ya8";
+    localStorage.setItem("token", token);
+  };
 
-    const handleSearch = () => {
-        // TODO: Implement search functionality
-        console.log(`Searching for students with name or ID containing: ${searchTerm}`);
-    };
+  const handleSearch = async () => {
+    try {
+      handleLogin();
+      console.log(process.env.SERVER_IP);
+      console.log(studentId);
+      const response = await axios.get(
+        `http://localhost:3000/api/students/getStudentDetailsByStudentID/${studentId}`,
+        {
+          headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+        }
+      );
+      console.log("API response:", response.data); // Keep this line to log the API response
+      setSearchResults([response.data.student]); // Wrap the student object in an array
+    } catch (error) {
+      console.error("Error searching for students:", error);
+    }
+  };
 
-    return (
-        <div className="user-search">
-            <h2>Search Students</h2>
-            <div className="search-bar">
-                <input type="text" placeholder="Enter student name or ID" value={searchTerm} onChange={handleInputChange} />
-                <button onClick={handleSearch}>Search</button>
+  return (
+    <div className="user-search">
+      <h2>Search Students</h2>
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Enter student name or ID"
+          value={studentId}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      <div className="search-results">
+        {Array.isArray(searchResults) &&
+          searchResults.map((student) => (
+            <div key={student._id} className="student-details">
+              <p>Student ID: {student.studentId}</p>
+              <p>Email: {student.email}</p>
+              <p>First Name : {student.firstName}</p>
+              <p>Last Name : {student.lastName}</p>
             </div>
-            {/* Add search results here */}
-        </div>
-    );
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default UserSearch;
