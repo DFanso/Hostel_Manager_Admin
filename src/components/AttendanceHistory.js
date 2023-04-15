4e07b9c169d73572bbc7cf8f5c1e7b9806d64aa9
VidwaDeSeram <vidwadeseram2002@gmail.com>
2023-04-15 16:56:17 +0530

    attendance history done!

--- src/components/AttendanceHistory.js
+++ src/components/AttendanceHistory.js
@@ -1,31 +1,71 @@
-import React, { useState } from 'react';
-import './AttendanceHistory.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./AttendanceHistory.css";
 
 function AttendanceHistory(props) {
-    const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
+  const [attendanceRecords, setAttendanceRecords] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
-    const handleSearch = (event) => {
-        event.preventDefault();
-        props.onSearch(searchTerm);
-    };
+  const fetchAttendanceRecords = async (studentId, dataCount) => {
+    try {
+      console.log(studentId, dataCount);
+      const token =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDM1MDJjMzY5MjI4MTk0MDE2NjY2MmQiLCJpYXQiOjE2ODE1NTQ2NjMsImV4cCI6MTY4MTU1ODI2M30.lCdWumBTBWmDPhk2WDdUs5gFjsYmo-8hsWWbUeIwbLc";
+      const response = await axios.get(
+        `http://localhost:3000/api/students/${searchTerm}?dataCount=${dataCount}`,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      setAttendanceRecords(response.data.attendanceRecords);
+    } catch (error) {
+      console.error("Error fetching attendance records:", error);
+    }
+  };
 
-    return (
-        <div className="attendance-history">
-            <h2>Attendance History</h2>
-            <form className="search-bar">
-                <input
-                    type="text"
-                    placeholder="Search..."
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
-                <button type="submit" onClick={handleSearch} style={{ backgroundColor: '#1e90ff' }}>
-                    Search
-                </button>
-            </form>
-            {/* render attendance history table */}
-        </div>
-    );
+  useEffect(() => {
+    fetchAttendanceRecords(props.studentId, currentPage);
+  }, [searchTerm, currentPage]);
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    // Add any additional search functionality here
+  };
+
+  return (
+    <div className="attendance-history">
+      <h2>Attendance History</h2>
+      <form className="search-bar" onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button type="submit" style={{ backgroundColor: "#1e90ff" }}>
+          Search
+        </button>
+      </form>
+      {/* render attendance history table */}
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {attendanceRecords.map((record) => (
+            <tr key={record._id}>
+              <td>{new Date(record.timestamp).toLocaleDateString()}</td>
+              <td>{record.action}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default AttendanceHistory;
