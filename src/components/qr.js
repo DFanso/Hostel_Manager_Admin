7e34b71ecfa1d1d34d4f9ce0beb4a3af3bf564fe
VidwaDeSeram <vidwadeseram2002@gmail.com>
2023-04-18 20:29:24 +0530

    create QR done

--- src/components/qr.js
+++ src/components/qr.js
@@ -1,14 +1,56 @@
-import React from 'react';
-import './qr.css';
+import React, { useState } from "react";
+import "./qr.css";
+import QRCode from "qrcode";
+import axios from "axios";
 
 function GenerateQRButton(props) {
-    return (
-        <div class="qr">
-            <button className="generate-qr-button" onClick={props.onClick}>
-                Generate QR
-            </button>
-        </div>
-    );
+  const [qrCodeDataURL, setQRCodeDataURL] = useState(null);
+
+  const generateQRCode = async () => {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await axios.get(
+        "http://localhost:3000/api/guardian/CreateQRCode",
+        {
+          headers: {
+            token: `${token}`,
+          },
+        }
+      );
+      const qrToken = response.data.qrToken;
+      const dataURL = await QRCode.toDataURL(qrToken);
+      setQRCodeDataURL(dataURL);
+    } catch (error) {
+      console.error("Failed to generate QR code:", error);
+    }
+  };
+
+  const downloadQRCode = () => {
+    if (!qrCodeDataURL) return;
+
+    const link = document.createElement("a");
+    link.href = qrCodeDataURL;
+    link.download = "qr-code.png";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return (
+    <div className="qr">
+      <button className="generate-qr-button" onClick={generateQRCode}>
+        Generate QR
+      </button>
+      {qrCodeDataURL && (
+        <>
+          <img src={qrCodeDataURL} alt="QR code" />
+          <button className="download-qr-button" onClick={downloadQRCode}>
+            Download QR
+          </button>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default GenerateQRButton;
